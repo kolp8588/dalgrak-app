d43cde40ad5728bf2bc3835f83b95774409a1155
kolp8588 <67845932+kolp8588@users.noreply.github.com>
2020-08-24 19:27:56 +0900

    Update index.js

--- src/screens/DargrakBasicScreen/index.js
+++ src/screens/DargrakBasicScreen/index.js
@@ -1,63 +1,26 @@
-import React, { Component } from "react";
-import { View, Text } from "react-native";
-import * as firebase from "firebase";
+import { connect } from "react-redux";
+import Container from "./container";
+import { actionCreators as dalgrakActions } from "../../redux/modules/dalgrak";
 
-import DargrakBasicScreen from "./presenter";
+const mapStateToProps = (state, ownProps) => {
+  const {
+    dalgrak: { category },
+  } = state;
 
-class Container extends Component {
-  state = {
-    isSelected: false,
-    depth: 0,
-    parentCategory: null,
-    item: null,
-    category: null,
+  return {
+    category,
   };
+};
 
-  render() {
-    return (
-      <DargrakBasicScreen
-        {...this.state}
-        pickPhoto={this._pickPhoto}
-        approvePhoto={this._approvePhoto}
-        pickCategory={this._pickCategory}
-      />
-    );
-  }
-  _pickItem = (item) => {
-    this.setState({
-      pickedItem: item,
-    });
+const mapDispatchToProps = (dispatch, ownProps) => {
+  return {
+    getCategories: (parent) => {
+      return dispatch(dalgrakActions.getCategories(parent));
+    },
+    refreshStates: () => {
+      return dispatch(dalgrakActions.refreshStates());
+    },
   };
-  _pickCategory = (depth) => {
-    firebase
-      .database()
-      .ref("categories")
-      .orderByChild("depth")
-      .equalTo(depth)
-      .on("value", (snapshot) => {
-        let result = [];
-        snapshot.forEach((snap) => {
-          const item = snap.val();
-          item.key = snap.key;
-          result.push(item);
-        });
-        const {
-          navigation: { navigate },
-        } = this.props;
-        navigate("Category", {
-          depth: depth,
-          categories: result,
-        });
-      });
-  };
+};
 
-  _approveItem = () => {
-    const {
-      navigation: { navigate },
-    } = this.props;
-    const { pickedItem } = this.state;
-    navigate("UploadPhoto", { item: pickedItem });
-  };
-}
-
-export default Container;
+export default connect(mapStateToProps, mapDispatchToProps)(Container);
