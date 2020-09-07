d0fa6c662a577d583d48fc3320092361518e12e1
kolp8588 <67845932+kolp8588@users.noreply.github.com>
2020-09-07 16:50:54 +0900

    Create container.js

---
+++ src/components/Dalgrak/container.js
@@ -0,0 +1,46 @@
+import React, { Component } from "react";
+import { View, Text } from "react-native";
+import PropTypes from "prop-types";
+import Dalgrak from "./presenter";
+
+class Container extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  static propTypes = {};
+  render() {
+    return (
+      // <Dalgrak
+      //   handlePress={this._handlePress}
+      //   {...this.props}
+      //   {...this.state}
+      // />
+      <View>
+        <Text>HEY!</Text>
+      </View>
+    );
+  }
+  _handlePress = () => {
+    const { dispatchLike } = this.props;
+    const { isLiked } = this.state;
+    dispatchLike(isLiked);
+    if (isLiked) {
+      this.setState((prevState) => {
+        return {
+          isLiked: false,
+          likeCount: prevState.likeCount - 1,
+        };
+      });
+    } else {
+      this.setState((prevState) => {
+        return {
+          isLiked: true,
+          likeCount: prevState.likeCount + 1,
+        };
+      });
+    }
+  };
+}
+
+export default Container;
