6203af6e06de8d17f705c5b7b6d5e7e121dca559
kolp8588 <67845932+kolp8588@users.noreply.github.com>
2020-09-08 19:58:04 +0900

    Update HomeRoute.js

--- src/routes/HomeRoute.js
+++ src/routes/HomeRoute.js
@@ -1,14 +1,33 @@
 import React, { Component } from "react";
-import { View, Text, Image } from "react-native";
-
+import { TouchableWithoutFeedback } from "react-native";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 import { createStackNavigator } from "@react-navigation/stack";
 import FeedScreen from "../screens/FeedScreen";
 const Stack = createStackNavigator();
 class HomeRoute extends Component {
   render() {
+    const { navigation } = this.props;
     return (
       <Stack.Navigator>
-        <Stack.Screen name="feed" component={FeedScreen} />
+        <Stack.Screen
+          name="feed"
+          component={FeedScreen}
+          options={{
+            title: "Feed",
+            headerRight: () => (
+              <TouchableWithoutFeedback
+                style={{ marginRight: 20 }}
+                onPress={() => navigation.navigate("Events")}
+              >
+                <MaterialCommunityIcons
+                  name={"bell"}
+                  size={20}
+                  style={{ marginRight: 15 }}
+                />
+              </TouchableWithoutFeedback>
+            ),
+          }}
+        />
       </Stack.Navigator>
     );
   }
