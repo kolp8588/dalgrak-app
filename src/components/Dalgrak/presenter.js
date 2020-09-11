e3a39669ccee817a332c7ee269d448c8dc43e197
kolp8588 <67845932+kolp8588@users.noreply.github.com>
2020-09-11 17:22:32 +0900

    Update presenter.js

--- src/components/Dalgrak/presenter.js
+++ src/components/Dalgrak/presenter.js
@@ -9,8 +9,8 @@ import {
   StyleSheet,
 } from "react-native";
 import FadeIn from "react-native-fade-in-image";
-import { useNavigation } from "@react-navigation/native";
 import { COLORS, FONTS } from "../../constants";
+import { useNavigation } from "@react-navigation/native";
 
 const { width, height } = Dimensions.get("window");
 
@@ -19,88 +19,60 @@ class Dalgrak extends Component {
     const { navigation } = this.props;
     return (
       <View style={styles.dalgrak}>
-        <View
-          style={{
-            flexDirection: "row",
-          }}
+        <TouchableOpacity
+          onPressOut={() =>
+            navigation.navigate("dalgrak", { id: this.props.idx })
+          }
         >
-          <FadeIn>
-            <Image
-              source={{ uri: this.props.imageUrl }}
-              style={{ width: 100, height: 100 }}
-            />
-          </FadeIn>
           <View
             style={{
-              marginLeft: 20,
+              flexDirection: "row",
             }}
           >
-            <Text
+            <FadeIn>
+              <Image
+                source={{ uri: this.props.imageUrl }}
+                style={{ width: 100, height: 100 }}
+              />
+            </FadeIn>
+            <View
               style={{
-                fontSize: FONTS.SIZE.CONTENTS,
-                color: COLORS.DALGRAK,
+                marginLeft: 20,
               }}
             >
-              {this.props.category}
-            </Text>
-            <Text
-              style={{
-                fontSize: FONTS.SIZE.CONTENTS,
-              }}
-            >
-              {this.props.quantity} {this.props.unit}
-            </Text>
-            <Text
-              style={{
-                fontSize: FONTS.SIZE.CONTENTS,
-              }}
-            >
-              마감일 : {this.props.parseDate(this.props.date)}
-            </Text>
-            <Text
-              style={{
-                fontSize: FONTS.SIZE.CONTENTS,
-              }}
-            >
-              참여업체 : {this.props.participants}
-            </Text>
+              <Text
+                style={{
+                  fontSize: FONTS.SIZE.CONTENTS,
+                  color: COLORS.DALGRAK,
+                }}
+              >
+                {this.props.category}
+              </Text>
+              <Text
+                style={{
+                  fontSize: FONTS.SIZE.CONTENTS,
+                }}
+              >
+                {this.props.quantity} {this.props.unit}
+              </Text>
+              <Text
+                style={{
+                  fontSize: FONTS.SIZE.CONTENTS,
+                }}
+              >
+                마감일 : {this.props.parseDate(this.props.date)}
+              </Text>
+              <Text
+                style={{
+                  fontSize: FONTS.SIZE.CONTENTS,
+                }}
+              >
+                참여업체 : {this.props.participants}
+              </Text>
+            </View>
           </View>
-        </View>
+        </TouchableOpacity>
       </View>
-      // <View style={styles.photo}>
-      //   <FadeIn>
-      //     <Image
-      //       source={{ uri: this.props.file }}
-      //       style={{ width, height: props.is_vertical ? 600 : 300 }}
-      //     />
-      //   </FadeIn>
-      //   <View style={styles.photoMeta}>
-      //     <View style={styles.comment}>
-      //       <Text style={styles.commentAuthor}>
-      //         {props.creator.username}{" "}
-      //         <Text style={styles.message}>{props.caption}</Text>
-      //       </Text>
-      //     </View>
-      //     {props.comments.length > 0 && (
-      //       <TouchableOpacity
-      //         onPressOut={() => navigation.navigate("Comments")}
-      //       >
-      //         <View style={styles.commentsLink}>
-      //           {this.props.comments.length === 1 ? (
-      //             <Text style={styles.linkText}>View 1 comment</Text>
-      //           ) : (
-      //             <Text style={styles.linkText}>
-      //               View all {this.props.comments.length} comments
-      //             </Text>
-      //           )}
-      //         </View>
-      //       </TouchableOpacity>
-      //     )}
-      //     <Text style={styles.dateText}>
-      //       {props.natural_time.toUpperCase()}
-      //     </Text>
-      //   </View>
-      // </View>
     );
   }
 }
@@ -163,4 +135,8 @@ const styles = StyleSheet.create({
   },
 });
 
-export default Dalgrak;
+export default function (props) {
+  const navigation = useNavigation();
+
+  return <Dalgrak {...props} navigation={navigation} />;
+}
